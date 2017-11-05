5155eba7a18b5a142f795edc2cc37499e7930098
Jaakko Husso <jaakko.husso@futurice.com>
2017-11-05 19:06:27 +0200

    Initial commit

---
+++ lib/hexgrid.ex
@@ -0,0 +1,18 @@
+defmodule Hexgrid do
+  @moduledoc """
+  Documentation for Hexgrid.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hexgrid.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hexgrid_test.exs
@@ -0,0 +1,8 @@
+defmodule HexgridTest do
+  use ExUnit.Case
+  doctest Hexgrid
+
+  test "greets the world" do
+    assert Hexgrid.hello() == :world
+  end
+end
