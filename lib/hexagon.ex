184a25cc090e9fcd9b3c6b424f4a2d4092f157fe
Jaakko Husso <jaakko.husso@futurice.com>
2017-11-06 23:44:42 +0200

    Add better documentation and fix rotations.

--- lib/hexagon.ex
+++ lib/hexagon.ex
@@ -2,6 +2,22 @@ defmodule Hexagon do
   @moduledoc """
   Hexagon module that represents Hexagon tiles using cube coordinates.
   Instead of names x, y, z this module uses names q, r, s.
+
+  Axes are aligned as follows:
+  ```
+             -s
+             .^.
+          .´     `.
+    +r .´           `. +q
+      |               |
+      .               .
+      |               |
+    -q `.           .´ -r
+          `.     .´
+             `.´
+             +s
+
+  ```
   """
 
   defstruct q: 0, r: 0, s: 0
@@ -60,22 +76,83 @@ defmodule Hexagon do
     }
   end
 
-  @doc """
-  Rotate cube coordinates to given direction `:left` or `:right`.
-  Returns a new rotated Hexagon.
+  @doc ~S"""
+  Rotate cube coordinates 60° to given direction `:left` or `:right`.
+  Returns a new Hexagon at rotated position.
+
+  Rotations visualized
+  ```
+
+           .^.     .^.     .^.
+        .´2   0`.´     `.´0   2`.
+        |       |       |       |
+       .^. -2  .^.     .^. -2  .^.
+    .´     `.´     `.´     `.´     `.
+    |       |       |       |       |
+    `.     .^.     .^.     .^.     .^.
+       `.´     `.´r   q`.´     `.´-2  2`.
+        |       |       |       |       |
+        `.     .^.  s  .^.     .^.  0  .´
+           `.´     `.´     `.´     `.´
+            |       |       |       |
+            `.     .^.     .^.     .´
+               `.´     `.´     `.´
+
+  ```
 
   ## Examples:
-    iex> a = %Hexagon{q: 1, r: 2, s: 3}
+    iex> a = %Hexagon{q: 2, r: 0, s: -2}
     iex> Hexagon.rotate(a, :left)
-    %Hexagon{q: -3, r: -1, s: -2}
+    %Hexagon{q: 0, r: 2, s: -2}
     iex> Hexagon.rotate(a, :right)
-    %Hexagon{q: -2, r: -3, s: -1}
+    %Hexagon{q: 2, r: -2, s: 0}
   """
   def rotate(%Hexagon{} = a, direction) do
     case direction do
-      :left -> %Hexagon{q: -a.s, r: -a.q, s: -a.r}
-      :right -> %Hexagon{q: -a.r, r: -a.s, s: -a.q}
+      :left -> %Hexagon{q: -a.r, r: -a.s, s: -a.q}
+      :right -> %Hexagon{q: -a.s, r: -a.q, s: -a.r}
     end
   end
 
+  @doc ~S"""
+  Find direction of neighbouring hexagons based on given direction.
+  Returns a new Hexagon representing the direction as a vector.
+
+  Directions are enumerated in following order:
+  ```
+
+  :north_west        :north_east
+               .^.
+            .´r   q`.
+    :west   |       |  :east
+            `.  s  .´
+               `.´
+  :south_west        :south_east
+
+  ```
+
+  ## Examples:
+    iex> Hexagon.neighbour_direction(:north_east)
+    %Hexagon{q: 1, r: 0, s: -1}
+    iex> Hexagon.neighbour_direction(:east)
+    %Hexagon{q: 1, r: -1, s: 0}
+    iex> Hexagon.neighbour_direction(:south_east)
+    %Hexagon{q: 0, r: -1, s: 1}
+    iex> Hexagon.neighbour_direction(:south_west)
+    %Hexagon{q: -1, r: 0, s: 1}
+    iex> Hexagon.neighbour_direction(:west)
+    %Hexagon{q: -1, r: 1, s: 0}
+    iex> Hexagon.neighbour_direction(:north_west)
+    %Hexagon{q: 0, r: 1, s: -1}
+  """
+  def neighbour_direction(direction) do
+    case direction do
+      :north_east -> %Hexagon{q: 1, r: 0, s: -1}
+      :east -> %Hexagon{q: 1, r: -1, s: 0}
+      :south_east -> %Hexagon{q: 0, r: -1, s: 1}
+      :south_west -> %Hexagon{q: -1, r: 0, s: 1}
+      :west -> %Hexagon{q: -1, r: 1, s: 0}
+      :north_west -> %Hexagon{q: 0, r: 1, s: -1}
+    end
+  end
 end
