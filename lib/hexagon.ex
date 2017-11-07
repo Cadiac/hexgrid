6362f96623aa01fe343818b6693bd828b0851027
Jaakko Husso <jaakko.husso@futurice.com>
2017-11-08 00:35:26 +0200

    Format doctests better. Change all tests to be correct cube coordinate hexagons.

--- lib/hexagon.ex
+++ lib/hexagon.ex
@@ -3,6 +3,9 @@ defmodule Hexagon do
   Hexagon module that represents Hexagon tiles using cube coordinates.
   Instead of names x, y, z this module uses names q, r, s.
 
+  Cube coordinates have a constraint `q + r + s = 0`,
+  even with floating point cube coordinates. This has to be always respected.
+
   Axes are aligned as follows:
   ```
              -s
@@ -27,10 +30,10 @@ defmodule Hexagon do
   Returns a new Hexagon with added coordinates.
 
   ## Examples:
-    iex> a = %Hexagon{q: 1, r: 2, s: 3}
-    iex> b = %Hexagon{q: 3, r: 2, s: 1}
+    iex> a = %Hexagon{q: 1, r: -2, s: 1}
+    iex> b = %Hexagon{q: 3, r: -2, s: -1}
     iex> Hexagon.add(a, b)
-    %Hexagon{q: 4, r: 4, s: 4}
+    %Hexagon{q: 4, r: -4, s: 0}
   """
   def add(%Hexagon{} = a, %Hexagon{} = b) do
     %Hexagon{
@@ -45,8 +48,8 @@ defmodule Hexagon do
   Returns a new subtracted Hexagon.
 
   ## Examples:
-    iex> a = %Hexagon{q: 1, r: 2, s: 3}
-    iex> b = %Hexagon{q: 3, r: 2, s: 1}
+    iex> a = %Hexagon{q: 1, r: -2, s: 1}
+    iex> b = %Hexagon{q: 3, r: -2, s: -1}
     iex> Hexagon.subtract(a, b)
     %Hexagon{q: -2, r: 0, s: 2}
   """
@@ -64,15 +67,15 @@ defmodule Hexagon do
   Returns a new scaled Hexagon.
 
   ## Examples:
-    iex> a = %Hexagon{q: 1, r: 2, s: 3}
+    iex> a = %Hexagon{q: 1, r: -2, s: 1}
     iex> Hexagon.scale(a, 3)
-    %Hexagon{q: 3, r: 6, s: 9}
+    %Hexagon{q: 3, r: -6, s: 3}
   """
-  def scale(%Hexagon{} = a, k) when is_integer(k) do
+  def scale(%Hexagon{} = h, k) when is_integer(k) do
     %Hexagon{
-      q: a.q * k,
-      r: a.r * k,
-      s: a.s * k
+      q: h.q * k,
+      r: h.r * k,
+      s: h.s * k
     }
   end
 
@@ -107,10 +110,10 @@ defmodule Hexagon do
     iex> Hexagon.rotate(a, :right)
     %Hexagon{q: 2, r: -2, s: 0}
   """
-  def rotate(%Hexagon{} = a, direction) do
+  def rotate(%Hexagon{} = h, direction) do
     case direction do
-      :left -> %Hexagon{q: -a.r, r: -a.s, s: -a.q}
-      :right -> %Hexagon{q: -a.s, r: -a.q, s: -a.r}
+      :left -> %Hexagon{q: -h.r, r: -h.s, s: -h.q}
+      :right -> %Hexagon{q: -h.s, r: -h.q, s: -h.r}
     end
   end
 
@@ -133,14 +136,19 @@ defmodule Hexagon do
   ## Examples:
     iex> Hexagon.directions(:north_east)
     %Hexagon{q: 1, r: 0, s: -1}
+
     iex> Hexagon.directions(:east)
     %Hexagon{q: 1, r: -1, s: 0}
+
     iex> Hexagon.directions(:south_east)
     %Hexagon{q: 0, r: -1, s: 1}
+
     iex> Hexagon.directions(:south_west)
     %Hexagon{q: -1, r: 0, s: 1}
+
     iex> Hexagon.directions(:west)
     %Hexagon{q: -1, r: 1, s: 0}
+
     iex> Hexagon.directions(:north_west)
     %Hexagon{q: 0, r: 1, s: -1}
   """
@@ -172,30 +180,50 @@ defmodule Hexagon do
 
   ```
 
+  Neighbours visualized
+  ```
+
+           .^.     .^.
+        .´1   1`.´0   2`.
+        |       |       |
+       .^. -2  .^. -2  .^.
+    .´1   0`.´0   1`.´-1  2`.
+    |       |       |       |
+    `. -1  .^. -1  .^. -1  .´
+       `.´0   0`.´-1  1`.´
+        |       |       |
+        `.  0  .^.  0  .´
+           `.´     `.´
+
+  ```
+
   ## Examples:
-    iex> a = %Hexagon{q: 1, r: 2, s: 3}
-    iex> Hexagon.neighbour(a, :south_west)
-    %Hexagon{q: 0, r: 2, s: 4}
+
+    iex> a = %Hexagon{q: 1, r: 0, s: -1}
     iex> Hexagon.neighbour(a, :north_east)
-    %Hexagon{q: 2, r: 2, s: 2}
+    %Hexagon{q: 2, r: 0, s: -2}
+    iex> Hexagon.neighbour(a, :north_west)
+    %Hexagon{q: 1, r: 1, s: -2}
   """
-  def neighbour(%Hexagon{} = a, direction) do
-    Hexagon.add(a, Hexagon.directions(direction))
+  def neighbour(%Hexagon{} = h, direction) do
+    Hexagon.add(h, Hexagon.directions(direction))
   end
 
   @doc ~S"""
   Returns the distance of hexagon `a` from origo as an integer.
 
   ## Examples:
-    iex> a = %Hexagon{q: 1, r: 2, s: 3}
+
+    iex> a = %Hexagon{q: 1, r: -2, s: 1}
     iex> Hexagon.length(a)
-    3
-    iex> b = %Hexagon{q: -2, r: 2, s: -3}
+    2
+
+    iex> b = %Hexagon{q: -2, r: -3, s: 5}
     iex> Hexagon.length(b)
-    3
+    5
   """
-  def length(%Hexagon{} = a) do
-    div((abs(a.q) + abs(a.r) + abs(a.s)), 2)
+  def length(%Hexagon{} = h) do
+    div((abs(h.q) + abs(h.r) + abs(h.s)), 2)
   end
 
   @doc ~S"""
@@ -203,12 +231,43 @@ defmodule Hexagon do
   Return value is an integer value.
 
   ## Examples:
-    iex> a = %Hexagon{q: 1, r: 2, s: 3}
-    iex> b = %Hexagon{q: -2, r: 2, s: -3}
+    iex> a = %Hexagon{q: 1, r: -2, s: 1}
+    iex> b = %Hexagon{q: -2, r: -3, s: 5}
     iex> Hexagon.distance(a, b)
     4
   """
   def distance(%Hexagon{} = a, %Hexagon{} = b) do
     Hexagon.length(Hexagon.subtract(a, b))
+  end
+
+  @doc ~S"""
+  Rounds up an hexagon with float values to integer coordinates.
+
+  ## Examples:
+    iex> a = %Hexagon{q: 1.5, r: -2.25, s: 0.75}
+    iex> Hexagon.round_hex(a)
+    %Hexagon{q: 1, r: -2, s: 1}
+
+    iex> b = %Hexagon{q: 1.2, r: 2.5, s: -3.7}
+    iex> Hexagon.round_hex(b)
+    %Hexagon{q: 1, r: 3, s: -4}
+  """
+  def round_hex(%Hexagon{} = h) do
+    q = round(h.q)
+    r = round(h.r)
+    s = round(h.s)
+
+    q_diff = abs(q - h.q)
+    r_diff = abs(r - h.r)
+    s_diff = abs(s - h.s)
+
+    cond do
+      (q_diff > r_diff && q_diff > s_diff) ->
+        %Hexagon{q: -r - s, r: r, s: s}
+      (r_diff > s_diff) ->
+        %Hexagon{q: q, r: -q - s, s: s}
+      true ->
+        %Hexagon{q: q, r: r, s: -q - r}
+    end
   end
 end
