92f0f129f572774a8d72f87fc108744d939ad06a
Jaakko Husso <jaakko.husso@futurice.com>
2017-11-26 02:49:01 +0200

    Add methods for drawing hexgrid

--- lib/hexgrid.ex
+++ lib/hexgrid.ex
@@ -3,6 +3,9 @@ defmodule Hexgrid do
   Documentation for Hexgrid.
   """
 
+  require Logger
+  alias Hexagon
+
   @doc """
   Hello world.
 
@@ -14,5 +17,102 @@ defmodule Hexgrid do
   """
   def hello do
     :world
+  end
+
+  defp create_row(mapset, _r, q) when q <= 0 do
+    mapset
+  end
+
+  defp create_row(mapset, r, q) do
+    MapSet.put(mapset, %Hexagon{q: q, r: r, s: -q - r})
+    |> create_row(r, q - 1)
+  end
+
+  def create(mapset, r, _q) when r <= 0 do
+    mapset
+  end
+
+  def create(mapset, r, q) do
+    mapset = create_row(mapset, r, q)
+    create(mapset, r - 1, q)
+  end
+
+  def draw() do
+    Logger.info "
+           .^.     .^.
+        .´1   1`.´0   2`.
+        |       |       |
+       .^. -2  .^. -2  .^.
+    .´1   0`.´0   1`.´-1  2`.
+    |       |       |       |
+    `. -1  .^. -1  .^. -1  .´
+       `.´0   0`.´-1  1`.´
+        |       |       |
+        `.  0  .^.  0  .´
+           `.´     `.´
+    "
+  end
+
+  defp hexagon_row_string(i, type) when i == 0 do
+    case type do
+      :top -> "   .^.   "
+      :top_coords -> ".´<%= r %> <%= q %>`."
+      :middle -> "|       |"
+      :bottom_coords -> "^. <%= s %> .^"
+      :bottom -> "   `.´   "
+    end
+  end
+
+  defp hexagon_row_string(_, type) do
+    case type do
+      :top -> "  .^.   "
+      :top_coords -> "´<%= r %> <%= q %>`."
+      :middle -> "       |"
+      :bottom_coords -> ". <%= s %> .^"
+      :bottom -> "  `.´   "
+    end
+  end
+
+  defp draw_hexagon_row(%Hexagon{} = h, i, type) do
+    row_string = hexagon_row_string(i, type)
+
+    case type do
+      :top_coords -> row_string
+        |> EEx.eval_string([
+          r: h.r |> Integer.to_string |> String.pad_trailing(2),
+          q: h.q |> Integer.to_string |> String.pad_leading(2)
+        ])
+      :bottom_coords -> row_string
+        |> EEx.eval_string([
+        s: h.s |> Integer.to_string |> String.pad_leading(3)
+        ])
+      _default -> row_string
+    end
+  end
+
+  def draw(%MapSet{} = mapset) do
+    Logger.info Enum.join([
+      "\n",
+      mapset
+      |> Enum.with_index
+      |> Enum.map(fn({v, k})
+        -> draw_hexagon_row(v, k, :top) end),
+      mapset
+      |> Enum.with_index
+      |> Enum.map(fn({v, k})
+        -> draw_hexagon_row(v, k, :top_coords) end),
+      mapset
+      |> Enum.with_index
+      |> Enum.map(fn({v, k})
+        -> draw_hexagon_row(v, k, :middle) end),
+      mapset
+      |> Enum.with_index
+      |> Enum.map(fn({v, k})
+        -> draw_hexagon_row(v, k, :bottom_coords) end),
+      mapset
+      |> Enum.with_index
+      |> Enum.map(fn({v, k})
+        -> draw_hexagon_row(v, k, :bottom) end),
+    ], "\n")
   end
 end
